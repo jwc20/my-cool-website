b98f459d9f2e179c9091c9efba009f60f7940dbf
jwc20 <choij2015@gmail.com>
2022-02-13 22:03:17 -0800

    Added cat and fire images

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Welcome to my COOL WEBSITE";
 
+h2.appendChild(document.createElement('br'));
+h2.appendChild(document.createElement('br'));
+h2.appendChild(document.createElement('br'));
 document.querySelector("body").appendChild(h2);
 
 
+const fireTop = document.createElement("img");
+fireTop.src = './img/fire.gif';
+document.querySelector("body").appendChild(fireTop);
 
+
+const fatCat = document.createElement("img");
+fatCat.src = './img/fatcat.webp';
+document.querySelector("body").appendChild(fatCat);
+
+const fireBottom = document.createElement("img");
+fireBottom.src = './img/fire.gif';
+document.querySelector("body").appendChild(fireBottom);
