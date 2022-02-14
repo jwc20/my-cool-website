1059e012ff84ff694896f51e7377436876e82ea3
jwc20 <choij2015@gmail.com>
2022-02-13 22:09:54 -0800

    added About me

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Welcome to my COOL WEBSITE";
-
 h2.appendChild(document.createElement('br'));
 h2.appendChild(document.createElement('br'));
 h2.appendChild(document.createElement('br'));
@@ -11,7 +10,6 @@ const fireTop = document.createElement("img");
 fireTop.src = './img/fire.gif';
 document.querySelector("body").appendChild(fireTop);
 
-
 const fatCat = document.createElement("img");
 fatCat.src = './img/fatcat.webp';
 document.querySelector("body").appendChild(fatCat);
@@ -19,3 +17,12 @@ document.querySelector("body").appendChild(fatCat);
 const fireBottom = document.createElement("img");
 fireBottom.src = './img/fire.gif';
 document.querySelector("body").appendChild(fireBottom);
+
+const about = document.createElement("h2");
+about.textContent = "About me";
+document.querySelector("body").appendChild(about);
+
+const desc = document.createElement("h2");
+desc.textContent = "I love cats, programming, and pizza";
+document.querySelector("body").appendChild(desc);
+
