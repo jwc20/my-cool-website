417147eaea92608cf50af7e98dccf7d6cbbbf731
jwc20 <choij2015@gmail.com>
2022-02-13 20:59:50 -0800

    edited js file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Hello future employers!";
 
 document.querySelector("body").appendChild(h2);
 
